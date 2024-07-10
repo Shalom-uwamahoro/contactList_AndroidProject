4c2fd87a9e3f7860f8f252c1f7645e0970bf61e0
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-07-10 17:38:10 +0300

    added the contact list with user profile pictures

--- app/src/main/java/com/shalomu/mycontacts/ContactsAdapter.kt
+++ app/src/main/java/com/shalomu/mycontacts/ContactsAdapter.kt
@@ -1,35 +1,66 @@
 package com.shalomu.mycontacts
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.shalomu.mycontacts.databinding.ContactListItemBinding
 
-class ContactsAdapter(var contactsList: List<Contact>):
-    RecyclerView.Adapter<ContactsViewHolder>(){
+class ContactsAdapter ( var contactsList: List<Contact>):RecyclerView.Adapter<ContactsViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.contact_list_item, parent,false)
-        return ContactsViewHolder(itemView)
+        val binding =ContactListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ContactsViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
-        val contact = contactsList[position]
-        holder.tvName.text = contact.name
-        holder.tvPhoneNumber.text = contact.phoneNumber
-        holder.tvEmail.text = contact.email
+        val  contact = contactsList[position]
+        holder.binding.tvName.text=contact.name
+        holder.binding.tvPhoneNumber.text = contact.phoneNumber
+        holder.binding.tvEmail.text = contact.email
     }
 
     override fun getItemCount(): Int {
         return contactsList.size
     }
 }
-
-class ContactsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ContactsViewHolder(var binding: ContactListItemBinding):RecyclerView.ViewHolder(binding.root){
     val tvName = itemView.findViewById<TextView>(R.id.tvName)
     val tvPhoneNumber = itemView.findViewById<TextView>(R.id.tvPhoneNumber)
-    val tvEmail = itemView.findViewById<TextView>(R.id.tvEmail)
-
+    val tvEmail= itemView.findViewById<TextView>(R.id.tvEmail)
 }
+
+//
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//import com.shalomu.mycontacts.databinding.ContactListItemBinding
+//
+//class ContactsAdapter(var contactsList: List<Contact>): RecyclerView.Adapter<ContactsViewHolder>(){
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
+//        val binding = ContactListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return ContactsViewHolder(binding)
+//    }
+//
+//        override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+//            val  contact = contactsList[position]
+//            holder.binding.tvName.text=contact.name
+//            holder.binding.tvPhoneNumber.text = contact.phoneNumber
+//            holder.binding.tvEmail.text = contact.email
+//        }
+//
+//        override fun getItemCount(): Int {
+//            return contactsList.size
+//        }
+//
+//
+//
+//
+//class ContactsViewHolder(var binding: ContactListItemBinding, position: Int) {
+//    val tvName = itemView.findViewById<TextView>(R.id.tvName)
+//    val tvPhoneNumber = itemView.findViewById<TextView>(R.id.tvPhoneNumber)
+//    val tvEmail = itemView.findViewById<TextView>(R.id.tvEmail)
+//
+//}
+//
+//
